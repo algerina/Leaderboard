ed5424dd4cca98b86cdce0ebb6739c50cc35149a
Amel Khiri <53347966+algerina@users.noreply.github.com>
2021-11-17 00:45:24 +0100

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import './style.css';
 
 const players = [
-    {
-        player: 'player A',
-        score: 10, 
-    },
-    { 
-        player: 'player B', 
-        score: 20,
-    },
-    {
-        player: 'player C', 
-        score: 16,
-    },
+  {
+    player: 'player A',
+    score: 10,
+  },
+  {
+    player: 'player B',
+    score: 20,
+  },
+  {
+    player: 'player C',
+    score: 16,
+  },
 
 ];
 
-const scoresTable =  document.querySelector('#scoresTable');
+const scoresTable = document.querySelector('#scoresTable');
 players.forEach((element) => {
-    const li = document.createElement('li');
-    const text = document.createElement('span');
-    element.textContent = `${element.name}: ${element.score}`;
-    li.appendChild(text);
-    scoresTable.appendChild(li);
+  const li = document.createElement('li');
+  const text = document.createElement('span');
+  element.textContent = `${element.name}: ${element.score}`;
+  li.appendChild(text);
+  scoresTable.appendChild(li);
 });
