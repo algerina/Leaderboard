18a2cb38b67fbc9b358209bf769a38c2bba5c8ac
Amel Khiri <53347966+algerina@users.noreply.github.com>
2021-11-17 00:42:04 +0100

    Add arrow function as required

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
-import _ from 'lodash'; /* eslint-disable */
 import './style.css';
 
-element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+const players = [
+    {
+        player: 'player A',
+        score: 10, 
+    },
+    { 
+        player: 'player B', 
+        score: 20,
+    },
+    {
+        player: 'player C', 
+        score: 16,
+    },
+
+];
+
+const scoresTable =  document.querySelector('#scoresTable');
+players.forEach((element) => {
+    const li = document.createElement('li');
+    const text = document.createElement('span');
+    element.textContent = `${element.name}: ${element.score}`;
+    li.appendChild(text);
+    scoresTable.appendChild(li);
+});
